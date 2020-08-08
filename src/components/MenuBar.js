5aaab9774f4fb06f76c45c72ed461b3ffd2fd214
Marcos Velasco <devslife7@gmail.com>
2020-08-07 20:44:25 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -15,24 +15,34 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={e => props.changeMenu('profile')}
+        className={props.menuItem === 'profile' ? 'item active' : 'item'}
+        id="profile"
+        >
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={e => props.changeMenu('photos')}
+        className={props.menuItem === 'photos' ? 'item active' : 'item'}
+        id="photo"
+        >
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={e => props.changeMenu('cocktails')}
+        className={props.menuItem === 'cocktails' ? 'item active' : 'item'}
+        id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={e => props.changeMenu('pokemon')}
+        className={props.menuItem === 'pokemon' ? 'item active' : 'item'}
+        id="pokemon"
+        > 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
-
 }
 
 export default MenuBar
